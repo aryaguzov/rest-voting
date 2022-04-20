eb0fd3f99fc553cd8b91988eeacc18bac1e1f21a
aryaguzov <riahuzooleksii@gmail.com>
2022-04-20 13:15:30 +0300

    Initial commit

---
+++ src/main/java/com/graduation/restvoting/RestVotingApplication.java
@@ -0,0 +1,13 @@
+package com.graduation.restvoting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestVotingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/graduation/restvoting/RestVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.graduation.restvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
